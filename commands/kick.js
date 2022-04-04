a8751258ca330a1213f8c4a48f4402a0a85809d8
Hatakidev <avelsemenov@gmail.com>
2022-04-04 07:45:44 +0300

    fixed

--- commands/kick.js
+++ commands/kick.js
@@ -13,22 +13,20 @@ module.exports = {
         if (!member) {
           return interaction.reply('You need to mention the member you want to kick him');
         }
-    
-        if (!interaction.member.permissions.has('KICK_MEMBERS')) {
+        else if (!interaction.member.permissions.has('KICK_MEMBERS')) {
           return interaction.reply("I can't ban this user.");
-        }
-        const kickembed = new MessageEmbed()
-        .setColor('RED')
-        .setAuthor({name: `${interaction.member.username}`, iconURL: `${interaction.member.displayAvatarURL({dynamic: true})}`})
-        .setThumbnail(`${member.displayAvatarURL({dynamic: true})}`)
-        .setTitle(`${member.user.username} has been kicked`)
-        .setTimestamp()
-       
-    
-        return member.kick().then(() => {
-            interaction.reply({ embeds: [kickembed] });
-          });
+        }else {
+            const kickembed = new MessageEmbed()
+                .setColor('RED')
+                .setAuthor({name: `${interaction.member.username}`, iconURL: `${interaction.member.displayAvatarURL({dynamic: true})}`})
+                .setThumbnail(`${member.displayAvatarURL({dynamic: true})}`)
+                .setTitle(`${member.user.username} has been kicked`)
+                .setTimestamp()
 
+            return member.kick().then(() => {
+                interaction.reply({embeds: [kickembed]});
+            });
+        }
     },
     
 };
